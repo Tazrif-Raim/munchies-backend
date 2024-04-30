6a633b9f0e402888019f742cc117ab12bf019f54
Tazrif Raim <tazrifraim@gmail.com>
2024-04-30 10:54:20 +0600

    DAL UPDATE INGREDIENT, RECIPE, BRAND

---
+++ munchies-backend/DAL/DataAccessFactory.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<Brand, Guid, Brand> BrandData()
+        {
+            return new BrandRepo();
+        }
+
+        public static IRepo<Recipe, Guid, Recipe> RecipeData()
+        {
+            return new RecipeRepo();
+        }
+
+        public static IRepo<Ingredient, Guid, Ingredient> IngredientData()
+        {
+            return new IngredientRepo();
+        }
+    }
+}
