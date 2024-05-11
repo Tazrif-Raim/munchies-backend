561b6f65261ca697e1067c0f69992113a3e1a028
Tazrif Raim <tazrifraim@gmail.com>
2024-05-12 01:08:51 +0600

    .

---
+++ munchies-backend/packages/munchies-backend/BLL/Services/ResturantService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BAL.DTOs;
+using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services
+{
+    public class ResturantService
+    {
+        public static List<ResturantDTO> Get() { 
+        var data = DataAccessFactory.ResturantData().Read();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Resturant, ResturantDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<ResturantDTO>>(data);
+            return mapped;
+        }
+        public static ResturantDTO Get(int id) {
+        var data = DataAccessFactory.ResturantData().Read(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Resturant, ResturantDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<ResturantDTO>(data);
+            return mapped;
+        }
+    }
+}
---
+++ munchies-backend/packages/munchies-backend/munchies-backend/Controllers/RecipeController.cs
@@ -0,0 +1,115 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace munchies_backend.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        [HttpPost]
+        [Route("api/setRecipe")]
+        public async Task<HttpResponseMessage> SetRecipe()
+        {
+            var mime = "";
+            try
+            {
+                var provider = new MultipartFormDataStreamProvider(Path.GetTempPath());
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                var recipeJson = provider.FormData["recipe"];
+                var recipe = Newtonsoft.Json.JsonConvert.DeserializeObject<RecipeDTO>(recipeJson);
+
+                mime = provider.Contents[0].Headers.ContentType.MediaType;
+
+                var file = provider.FileData.Count > 0 ? provider.FileData[0] : null;
+                if(file != null)
+                {
+                    var videoFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.Headers.ContentDisposition.FileName.Trim('"'));
+                    
+                    var videoFilePath = Path.Combine("C:\\Users\\Admin\\Desktop\\munchies-backend\\munchies-backend\\videos", videoFileName);
+                    
+                    File.Move(file.LocalFileName, videoFilePath);
+
+                    recipe.videoPath = videoFilePath;
+                    recipe.Id = Guid.NewGuid();
+                    var ingredients = recipe.Ingredients;
+
+                    foreach (var item in ingredients)
+                    {
+                        item.Id = Guid.NewGuid();
+                    }
+
+                    recipe.Ingredients = ingredients;
+
+                    RecipeService.setRecipe(recipe);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "recipe received");
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new {message = ex.Message, m = mime});
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/recipeVideo")]
+
+        public async Task<HttpResponseMessage> GetRecipeVideo(string videoPath)
+        {
+            try
+            {
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                var videoStream = new FileStream(videoPath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096, useAsync: true);
+
+                response.Content = new StreamContent(videoStream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("video/mp4");
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+        /*public HttpResponseMessage GetRecipeVideo(string videoPath)
+        {
+            try
+            {
+                var video = File.ReadAllBytes(videoPath);
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                response.Content = new ByteArrayContent(video);
+                response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("video/mp4");
+                return response;
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new {message = ex.Message});
+            }
+        }*/
+
+        [HttpGet]
+        [Route("api/getRecipes")]
+        public HttpResponseMessage GetRecipes()
+        {
+            try
+            {
+                var recipes = RecipeService.getRecipes();
+                return Request.CreateResponse(HttpStatusCode.OK, recipes);
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new {message = ex.Message});
+            }
+        }
+    }
+}
