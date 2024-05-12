91d1b706d7847b32f78ca63ec79834e3d69eb04a
aminislam01 <aminislam01868@gmail.com>
2024-05-12 17:26:07 +0600

    a
    a

---
+++ munchies-backend/BLL/DTOs/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.DTOs
+{
+    public class UserDTO
+    {
+        public string Uname { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
